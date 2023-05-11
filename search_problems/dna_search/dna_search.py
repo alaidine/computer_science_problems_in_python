ab561acbc8e990d1b0164792ca543b3cc9bf9f8f
alaidine <aa.alimlinde@gmail.com>
2023-05-11 18:52:26 +0400

    im continuing the dna_search chapter

--- search_problems/dna_search/dna_search.py
+++ search_problems/dna_search/dna_search.py
@@ -2,3 +2,21 @@ from enum import IntEnum
 from typing import Typle, List
 
 Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide] # type alias codons
+Gene = List[Codon] # type alias
+
+
+
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s): # don't run off end!
+
+            return gene
+
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        gene.append(codon) # add codon to gene
+    return gene
